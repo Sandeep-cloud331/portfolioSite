eeff821bad6829f061d540182b9bca0f57adca68
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-02-16 23:56:51 +0530

    about me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import Hero from "./pages/hero.tsx";
+import { About } from "./pages/about.tsx";
 function App() {
   return (
     <>
       <Hero />
+      <About />
     </>
   )
 }
--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -6,7 +6,7 @@ function hero() {
     <div className="relative overflow-clip min-h-screen text-white bg-[linear-gradient(to_bottom,#000,#071E18_35%,#208A65_67%,#35FB8E_85%)]">
 
       <div
-        className="absolute bg-black w-[2400px] h-[1000px] rounded-[50%] left-1/2 -translate-x-1/2 
+        className="absolute bg-black w-[2400px] h-[1000px] rounded-[40%] left-1/2 -translate-x-1/2 
                       bg-[radial-gradient(closest-side,#000_85%,#249974)] top-[450px]
                       border-[1px] border-[#8CD6DE]/30"
       />
