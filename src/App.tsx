bacc57f2b7db6571b80e2050c1224c6244bd1bb7
Sandeep-cloud331 <sandeepcahahr@gmail.com>
2025-02-18 01:04:26 +0530

    projects page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Hero from "./pages/hero.tsx";
 import { About } from "./pages/about.tsx";
+import { Portfolio } from "./pages/portfolio.tsx";
 function App() {
   return (
     <>
       <Hero />
       <About />
+      <Portfolio />
     </>
   )
 }
